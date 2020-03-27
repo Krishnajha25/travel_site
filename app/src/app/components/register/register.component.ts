062cceaba74d6792b9c85682d46625b70f93e66a
Krishna Jha <krishnajharediff@gmail.com>
2020-03-27 11:44:00 +0530

    Few changes made

--- app/src/app/components/register/register.component.ts
+++ app/src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ValidateService } from '../../services/validate.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  first_name: String;
+  last_name: String;
+  email: String;
+  password: String;
+  confirm_password: String;
+
+  constructor(private validateService: ValidateService) { }
 
   ngOnInit(): void {
+  }
+
+  onRegisterSubmit(){
+    const user = {
+      first_name: this.first_name,
+      last_name: this.last_name,
+      email: this.email,
+      password: this.password
+    }
+
+    if(!this.validateService.validateRegister(user)){
+      console.log("Please enter the required fields")
+      return false;
+    }
+
+    if(!this.validateService.validateEmail(user.email)){
+      console.log("Please enter a valid email");
+      return false;
+    }
   }
 
 }
